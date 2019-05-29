66c044a53f0a7515252baf2bf3372992c20edf4f
Frederik Rabøl <frederik-rm@hotmail.com>
2019-05-29 09:09:31 +0200

    added feed items

--- pages/feed.vue
+++ pages/feed.vue
@@ -1,9 +1,11 @@
 <template>
   <section>
-    <article>
-      <h1>Hi!</h1>
+    <article v-for="item in feedItems" :key="item.id">
+      <figure>
+        <img :src="getImage(item)" alt="" />
+      </figure>
+      <p v-text="item.title"></p>
     </article>
-    {{ feed }}
   </section>
 </template>
 <script lang="ts">
@@ -33,6 +35,12 @@ export default class Feed extends Vue {
       }
     }
   }
+  get feedItems(): Object | null {
+    return get(this.feed, 'data.data.feed.data')
+  }
+  getImage(item): String | null {
+    return get(item, 'images.data[2].url')
+  }
   @feedNamespace.Getter('feed')
   feed
   @feedNamespace.Action('fetchFeed')
@@ -56,5 +64,39 @@ body {
   padding: 0;
   font-family: 'Barlow', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
     Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  grid-template: 1fr;
+  align-items: center;
+  justify-items: center;
+}
+
+section {
+  display: grid;
+  grid-gap: 1rem;
+  max-width: 1300px;
+  grid-template-columns: repeat(4, 1fr);
+  article {
+    background-color: #222;
+    display: grid;
+    grid-gap: 0.5rem;
+    grid-template: 350px auto / 1fr;
+    p {
+      display: flex;
+      flex-direction: column;
+      align-self: start;
+      padding: 0 0.5rem 0.5rem;
+    }
+    figure {
+      display: flex;
+      align-items: flex-end;
+      width: initial;
+      height: initial;
+      max-height: 200px;
+      overflow: hidden;
+      img {
+        height: auto;
+        max-width: 100%;
+      }
+    }
+  }
 }
 </style>
